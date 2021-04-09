b2d144c8593551765328bc5fd165c69575a76918
Joshua Adam Wilson <50850874+JoshuaAdamWilson@users.noreply.github.com>
2021-04-09 14:25:33 -0600

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/NavBar/NavBar"
+import Header from './components/Header/Header'
+import Particles from "react-particles-js"
 
 function App() {
   return (
+    <>
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: {
+                  "value": ["#7D8DF4", "#B069FF", "#9739FF", "#35c43e", "#27996f"]
+                }
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
+      <Header />
+    </>
   );
 }
 
