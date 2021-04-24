aa655bf3b0244a7f6df5b0bdef06c6fdbb4fd22b
Joshua Adam Wilson <50850874+JoshuaAdamWilson@users.noreply.github.com>
2021-04-24 15:53:42 -0600

    Added Facebook to Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,8 +1,9 @@
 import React from "react";
 import instagram from "../../images/instagram.jpg";
+import facebook from "../../images/facebook.jpg";
 // Fontawesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
 // REact pop-up box
 import { PopupboxManager, PopupboxContainer } from "react-popupbox";
 import "react-popupbox/dist/react-popupbox.css";
@@ -16,7 +17,7 @@ const Portfolio = () => {
           src={instagram}
           alt="Instagram Clone Project"
         />
-        <p>
+        <p className="popup-paragraph">
           Instagram is an American photo and video-sharing social networking
           service owned by Facebook, Inc.
         </p>
@@ -53,6 +54,50 @@ const Portfolio = () => {
     fadeInSpeed: 500,
   };
 
+  const openPopupboxFacebook = () => {
+    const content = (
+      <>
+        <img
+          className="portfolio-image-popupbox"
+          src={facebook}
+          alt="Facebook Clone Project"
+        />
+        <p className="popup-paragraph">
+          Facebook is an American online social media and social networking service based in Menlo Park, California, and a flagship service of the namesake company Facebook, Inc.
+        </p>
+        <p><b>ReactJS for Front End, Firebase for Back End</b></p>
+        <a
+          href="https://facebook-clone-5a681.web.app/"
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hyper-link"
+        >
+          <b>Demo</b>
+        </a>
+        <br />
+        
+        <a 
+          href="https://github.com/JoshuaAdamWilson/FacebookClone"
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hyper-link"
+        >
+          <b>Github</b>
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
+  const popupboxConfigFacebook = {
+    titleBar: {
+      enable: true,
+      text: "Facebook Clone Project",
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+  };
+
   return (
     <div id="portfolio" className="portfolio-wrapper">
       <div className="container">
@@ -70,9 +115,21 @@ const Portfolio = () => {
             <FontAwesomeIcon className="portfolio-icon" icon={faInstagram} />
           </div>
           {/*asdfasdf*/}
+          <div className="portfolio-image-box" onClick={openPopupboxFacebook}>
+            <img
+              className="portfolio-image"
+              src={facebook}
+              alt="Facebook Clone Project"
+            />
+            <div className="overflow"></div>
+            <i class="fab fa-facebook"></i>
+            <FontAwesomeIcon className="portfolio-icon" icon={faFacebook} />
+          </div>
+          {/* kjsdgfjaksdh */}
         </div>
       </div>
       <PopupboxContainer {...popupboxConfigInstagram} />
+      <PopupboxContainer {...popupboxConfigFacebook} />
     </div>
   );
 };
